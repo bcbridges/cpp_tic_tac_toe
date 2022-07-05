372dfc69477fdfe3570ef4d3105488a8ee116b89
bcbridges <bcb35b@gmail.com>
2022-07-05 12:27:11 -0600

    Added .gitignore for executable and added checkWin defn

--- main.cpp
+++ main.cpp
@@ -18,5 +18,35 @@ int main()
 
 // checkWin will check all possible combos of possible wins
 // 1 = winner, -1 = no winner continue, 0 = tie
+// this needs to be refactored - holy hell
+int checkWin()
+{
+    // Check Horizontals
+    if (square[1] == square[2] && square[2] == square[3])
+        return 1;
+    else if (square[4] == square[5] && square[5] == square[6])
+        return 1;
+    else if (square[7] == square[8] && square[8] == square[9])
+        return 1;
+    // Check Verticals
+    else if (square[1] == square[4] && square[4] == square[7])
+        return 1;
+    else if (square[2] == square[5] && square[5] == square[8])
+        return 1;
+    else if (square[3] == square[6] && square[6] == square[9])
+        return 1;
+    else if (square[1] == square[5] && square[5] == square[9])
+        return 1;
+    else if (square[3] == square[5] && square[5] == square[7])
+        return 1;
+    // maybe loop through array to check values against their index number and return a bool?
+    else if (square[1] != '1' && square[2] != '2', square[3] != '3'
+             && square[4] != '4' && square[5] != '5' && square[6] != '6'
+             && square[7] != '7' && square[8] != '8' && square[9] != '9')
+        return 0;
+    else
+        return -1;
+}
+
 
 // board will render the board on the console
